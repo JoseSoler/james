4296eb2b7ee326f26955bea84e7dc47a5e60e963
camille valley <camille.valley@gmail.com>
2013-06-05 13:33:11 +0200

    using min version of the js frameworks

--- src/main/webapp/js/controllers/QuestionConnectionController.js
+++ src/main/webapp/js/controllers/QuestionConnectionController.js
@@ -4,7 +4,7 @@ JamesApp.controller('QuestionConnectionController',
 function QuestionConnectionController($scope, $location, $http, $templateCache, $rootScope) {
 
     $scope.submitquestion = function() {
-	        $http({method: 'GET', url: 'http://l-lab-01.zanox-live.de:8080/james/rest/getQuestion?qId=' + $scope.questionId, cache: $templateCache}).
+	        $http({method: 'GET', url: 'http://labs.zanox.com:8080/james/rest/getQuestion?qId=' + $scope.questionId, cache: $templateCache}).
             success(function(data, status, headers, config) {
 			
 				// Persist the question id
--- src/main/webapp/js/controllers/QuestionController.js
+++ src/main/webapp/js/controllers/QuestionController.js
@@ -9,9 +9,9 @@ function QuestionController($scope, $location, $http, $templateCache, $rootScope
 	else
 		$scope.questionId = $rootScope.sharedVars.qId;
 	
-	$http({method: 'GET', url: 'http://l-lab-01.zanox-live.de:8080/james/rest/getQuestion?qId=' + $scope.questionId, cache: $templateCache}).
+	$http({method: 'GET', url: 'http://labs.zanox.com:8080/james/rest/getQuestion?qId=' + $scope.questionId, cache: $templateCache}).
 	success(function(data, status, headers, config) {
-		$scope.questionText = data;
+		$scope.questionText = data.question;
 	}).
 	error(function(data, status, headers, config) {
 		$rootScope.sharedVars.messages = "Request failed";
@@ -21,7 +21,7 @@ function QuestionController($scope, $location, $http, $templateCache, $rootScope
 	
     $scope.submitanswer = function() {
 	
-	        $http({method: 'GET', url: 'http://l-lab-01.zanox-live.de:8080/james/rest/setAnswer?qId=' + $scope.questionId + '&answer=' + $scope.answer, cache: $templateCache}).
+	        $http({method: 'GET', url: 'http://labs.zanox.com:8080/james/rest/setAnswer?qId=' + $scope.questionId + '&answer=' + $scope.answer, cache: $templateCache}).
             success(function(data, status, headers, config) {
 				$location.path('/questionsuccess'); 
             }).
